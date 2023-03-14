5c3d27c734f0c1d83a80dd58f968660e9bdd84fa
josedavila7452 <67973259+josedavila7452@users.noreply.github.com>
2023-03-14 12:25:47 -0500

    2 files modified, 3 added

---
+++ src/components/yulissa/yulissa.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yulissa',
+  standalone: true,
+  templateUrl: './yulissa.component.html',
+  styleUrls: ['./yulissa.component.css']
+})
+export class YulissaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { YulissaComponent } from './components/yulissa/yulissa.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, YulissaComponent],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+  <app-yulissa></app-yulissa>
   `,
 })
 export class App {
